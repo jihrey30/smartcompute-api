2c2df03e9c8ba9eec8e95f0973d11e3c77586dc2
Jihrey Pingol <jihrey.p@avense-tech.com>
2026-07-28 16:24:19 +0800

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,9 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'max-lines': ['warn', { max: 300, skipBlankLines: true, skipComments: true }],
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
